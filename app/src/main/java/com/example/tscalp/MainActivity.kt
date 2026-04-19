fc9eb4b41fd95889e98c30afd01f8d225fd54375
biomorf <biomorf.2017@gmail.com>
2026-04-19 19:24:46 +0700

    FIX paths

---
+++ app/src/main/java/com/example/tscalp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.tscalp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.tscalp.presentation.screens.orders.OrderScreen
+import com.example.tscalp.ui.theme.TScalpTheme
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TScalpTheme {
+                MainScreen()
+                }
+            }
+        }
+    }
+}
